e12e7505ef8dee57e3c042b984a0359062ede252
Jorgelduarte <duarte.junior@gmail.com>
2018-01-23 01:54:12 +0000

    Print each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,27 @@
 var request = require('request');
+var secrets = require("./secrets");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'jorgelduarte'
+        },
+    };
+
+    request(options, function(err, res, body) {
+        cb(err, body);
+    });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+    if (err) {
+        console.error("Error:", err);
+    }
+    var users = JSON.parse(result);
+    users.forEach(user => {
+       console.log(user.avatar_url) 
+    });
+});
