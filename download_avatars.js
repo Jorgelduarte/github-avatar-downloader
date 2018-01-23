0fdda978271227b9e7621650c453e9d7955fda5d
Jorgelduarte <duarte.junior@gmail.com>
2018-01-23 03:57:18 +0000

    Now, I can choose the repo by arguments in Node

--- download_avatars.js
+++ download_avatars.js
@@ -4,46 +4,46 @@ var fs = require('fs');
 var repo1 = process.argv[2];
 var repo2 = process.argv[3];
 if (repo1 === undefined  || repo2 === undefined) {
-    console.log ("----------------------------------------------");
-    console.log ("Please, give both arguments - <owner> <repo>  |");
-    console.log ("----------------------------------------------");
+  console.log("----------------------------------------------");
+  console.log("Please, give both arguments - <owner> <repo>  |");
+  console.log("----------------------------------------------");
 } else {
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+  console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+  function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-        headers: {
-            'User-Agent': 'jorgelduarte',
-            Authorization: 'token '+secrets.GITHUB_TOKEN
-        },
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'jorgelduarte',
+        Authorization: 'token ' + secrets.GITHUB_TOKEN
+      }
     };
 
     request(options, function(err, res, body) {
-        cb(err, body);
+      cb(err, body);
     });
-}
+  }
 
-getRepoContributors(repo1, repo2, function(err, result) {
+  getRepoContributors(repo1, repo2, function(err, result) {
     if (err) {
-        console.error("Error:", err);
+      console.error("Error:", err);
     }
     console.log(result);
     var users = JSON.parse(result);
     users.forEach(user => {
 
-        function downloadImageByURL(url, filePath) {     
-            request.get(url)
-               .on('error', function (err) {                                   
-                 throw err; 
-               })
-               .on('response', function (response) {                           
-                 console.log('Response Status Code: ', response.statusCode);
-               })
-               .pipe(fs.createWriteStream(filePath));
-        }
-        downloadImageByURL(user.avatar_url, "pic/" + user.login +".jpg")
+      function downloadImageByURL(url, filePath) {
+        request.get(url)
+          .on('error', function (err) {
+            throw err;
+          })
+          .on('response', function (response) {
+            console.log('Response Status Code: ', response.statusCode);
+          })
+          .pipe(fs.createWriteStream(filePath));
+      }
+      downloadImageByURL(user.avatar_url, "pic/" + user.login + ".jpg");
     });
-});
+  });
 }
